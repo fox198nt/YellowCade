b9d6bd9e983092fe8ee9c34482a2b93e45db58b3
Fox198nt <97329405+fox198nt@users.noreply.github.com>
2022-05-10 04:12:44 +0000

    stuff update

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,18 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return "online"
+    return '''
+    <!doctype html>
+    <html>
+    <head>
+    <link href="/yellowcade.css" rel="stylesheet">
+    <h1>YellowCade</h1>
+    <p>a discord bot mamade by fox198</p>
+    <p>something to help me develop the cat command</p>
+    <iframe src="https://www.youtube.com/embed/Q5u6MDQAG7I"></iframe>
+    </body>
+    </html>
+    '''
 
 def run():
     app.run(host="0.0.0.0", port=9090)
--- main.py
+++ main.py
@@ -1,28 +1,65 @@
 import discord
 import os
 import discord.ext
+import random
+import praw
 from discord.ext import commands
 
 client = discord.Client()
 
 client = commands.Bot(command_prefix = 'y?')
 
+reddit = praw.Reddit(client_id = "rOn9qtyJnviLZW73lDUVYQ",
+                    client_secret = "zcl6gbiYCpMaNUJoIe3s1A-f_bA_5Q",
+                    password = "dpccyc1663",
+                    username = "Fox198YT",
+                    user_agent = "python")
+
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='if anyone says y?help!'))
     print("online")
 
-@client.command(name='ping', description='see the bot latency')
+@client.command(description='see the bot latency')
 async def ping(ctx: commands.Context):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
-@client.command(name='deeznuts', description="deez nuts.")
+@client.command()
+async def cats(ctx):
+    subreddit = reddit.subreddit("memes")
+    subs = []
+    top = subreddit.top(limit = 5)
+    for submission in top: print(submission.title)
+    
+@client.command(description="deez nuts.")
 async def deeznuts(ctx):
     await ctx.send('deez nuts in yo jaw')
 
-@client.command(name='say', description="Sends what you want the bot to say.")
+@client.command()
+async def emoji(ctx):
+    emoji = ["😃", "😁", "😅", "🤣", "😭", "😉", "😗", "😘", "😍", "🥳", "🙃", "😜", "😇", "😏", "😌", "😐", "🤔", "🤫", "🥱", "🐧", "😱", "🙄", "😤", "🥺", "🙁", "🤐", "😨" ]
+    await ctx.send(random.choice(emoji))
+
+@client.command(description="Sends what you want the bot to say.")
 async def say(ctx, *, content):
     await ctx.send(content)
 
+@client.command(description="The Bot Server")
+async def server(ctx):
+    embed=discord.Embed(title="Bot Server", url="https://discord.gg/MjXNdEu7JC", description="Click the text above to join the server!", color=ffff80)
+    embed.set_footer(text="Yellowcade by fox198nt#5762")
+    await ctx.send(embed=embed)
+
+@client.command(description="Kicks a member")
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    if reason==None:
+      reason="No reason provided."
+    await ctx.guild.kick(member)
+    await ctx.send(f'Kicked user {member.mention}. Reason: {reason}')
+
+#couldn't add mute
+    
 import keep_alive
 keep_alive.keep_alive()
 client.run(os.getenv("TOKEN"))
